fd74cecf0017856352cf22a1dfcc1d5ef8fa5c39
JesGardiner <jes.gardiner21@gmail.com>
2024-08-02 13:27:38 +0100

    Moved the footer as a paragraph in the header

--- src/App.js
+++ src/App.js
@@ -8,20 +8,22 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <Weather />
+        <p>
+          This project was coded by Jessica Gardiner and is <a
+            href="https://github.com/JesGardiner/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a> and hosted on
+          <a
+            href="https://app.netlify.com/teams/jesgardiner/sites"
+            target="_blank"
+            rel="noreferrer"
+          > Netlify
+          </a>
+        </p>
       </header>
-      <footer>
-        This project was coded by Jessica Gardiner and is
-        <a href="https://github.com/JesGardiner/weather-react" target="_blank">
-          open-sourced on Github
-        </a>
-        and hosted on
-        <a
-          href="https://app.netlify.com/teams/jesgardiner/sites"
-          target="_blank"
-        >
-          Netlify
-        </a>
-      </footer>
     </div>
   );
 }
